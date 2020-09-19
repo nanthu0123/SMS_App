9b887d43363277a54a379472173febbf2feb7533
nanthu0123 <nanthu0123@gmail.com>
2020-09-19 23:06:36 +0530

    cilent information

---
+++ credentials.py
@@ -0,0 +1,5 @@
+# token and sid for user authentication
+account_sid = 'ACc99ed05ec8c554768c995aba84542f6d'
+auth_token = '206a3f441f03f35e1f56a6141e44753c'
+my_cell = '+916380151429'
+my_twilio = '+13345085047'
